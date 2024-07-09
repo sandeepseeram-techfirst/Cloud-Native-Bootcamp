6317dc062473d0e8fc5f4ccdc0d4752bee142379
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-07-09 12:44:05 +0530

    cloud-native-workshop

--- grafana/install-alloy.sh
+++ grafana/install-alloy.sh
@@ -4,7 +4,7 @@ sudo mkdir -p /etc/apt/keyrings/ && \
 wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null && \
 echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list && \
 sudo apt-get update && \
-sudo apt-get install alloy -y && \
+sudo apt-get install alloy -y && \ 
 
 # Modify the Alloy service configuration to listen on the desired port
 sudo sed -i -e 's/CUSTOM_ARGS=""/CUSTOM_ARGS="--server.http.listen-addr=0.0.0.0:12345"/' /etc/default/alloy && \
