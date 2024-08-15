7d13fb68ddb2f7accf43878c88c7de69548a191d
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-08-15 14:46:53 +0530

    blockchain python program

---
+++ Blockchain/Ethereum-Fundamentals/noteapp.sol
@@ -0,0 +1,25 @@
+
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+
+contract NoteApp{
+
+    // create a state variable to hold message
+    string public note;
+
+    // Constructor function initializes the state variable with initial message
+    constructor(string memory _note) {
+        note = _note;
+    }
+
+    // Function to read message
+    function viewMyNote() public view returns (string memory) {
+        return note;
+    }
+
+    // Function to set a new message
+    function setNote(string memory _note) public {
+        note = _note;
+    }
+}
