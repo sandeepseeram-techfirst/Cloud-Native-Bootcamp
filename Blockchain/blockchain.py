5ee71d5ce662bb049d82179cf8847f24635f9c1a
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-08-15 14:42:25 +0530

    blockchain python program

--- Blockchain/blockchain.py
+++ Blockchain/blockchain.py
@@ -1,3 +1,12 @@
 import hashlib
 import time
+
+class Block:
+    def __init__(self, index, previous_hash, timestamp, transactions, hash, nonce):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.timestamp = timestamp
+        self.transactions = transactions
+        self.hash = hash
+        self.nonce = nonce
  
