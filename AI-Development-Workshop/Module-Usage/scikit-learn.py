5c8a6948934221d4d60ae37a07ce66415de1284b
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-08-20 16:10:07 +0530

    scikit-learn-sample

---
+++ AI-Development-Workshop/Module-Usage/scikit-learn.py
@@ -0,0 +1,5 @@
+# Scikit-learn helps computers learn from data, like teaching a computer to guess your favorite color.
+
+from sklearn.linear_model import LinearRegression
+
+model = LinearRegression()
