331ef754e386343c5d1985705752cb42104dbdc4
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-08-20 16:14:55 +0530

    tensorflow-sample

---
+++ AI-Development-Workshop/Module-Usage/tensorflow.py
@@ -0,0 +1,8 @@
+# TensorFlow is like a gym trainer for AI. It helps computers learn by practicing over and over.
+
+import tensorflow as tf
+
+x = tf.constant([2.0, 3.0])
+y = tf.constant([5.0, 7.0])
+result = x + y
+print(result)
