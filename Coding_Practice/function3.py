f6c72bcfdd24ad271966e66b4b55c80e23d216bb
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-12-04 14:07:41 +0530

    coding practice - functions

--- Coding_Practice/function3.py
+++ Coding_Practice/function3.py
@@ -1,3 +1,10 @@
 def main():
   random_bag = create_random_bag()
   display_bag()
+
+
+def create_random_bag():
+   marble1 = create_random_marble()
+   marble2 = create_random_marble()
+   marble3 = create_random_marble()
+   return [marble1, marble2, marble3]
