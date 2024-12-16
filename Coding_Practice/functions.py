683dfc2e224d059a1a61b30eba2c84a67723266c
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-12-16 14:01:01 +0530

    coding practice - functions

---
+++ Coding_Practice/functions.py
@@ -0,0 +1,12 @@
+def print_coin_flip():
+    # The next line checks whether a random value is 0 or 1
+    if random.randrange(0, 2) == 0:
+        print('heads')
+    else:
+        print('tails')
+
+
+
+print_coin_flip()
+print_coin_flip()
+print_coin_flip()
