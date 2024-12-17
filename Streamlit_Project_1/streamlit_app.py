e7ac9b4eec919ed54b8d4f17ca97916bb67048c5
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-12-17 13:12:53 +0530

    streamlit - project

--- Streamlit_Project_1/streamlit_app.py
+++ Streamlit_Project_1/streamlit_app.py
@@ -3,3 +3,8 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import pydeck as pdk
+
+
+df = pd.read_csv('/usercode/dataset.csv')
+
+st.header("My First Streamlit Application")
