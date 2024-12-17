d706c00fc3ec76ae7079a596b85dd5cce5c1c946
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-12-17 13:14:01 +0530

    streamlit - project

--- Streamlit_Project_1/streamlit_app.py
+++ Streamlit_Project_1/streamlit_app.py
@@ -8,3 +8,19 @@ import pydeck as pdk
 df = pd.read_csv('/usercode/dataset.csv')
 
 st.header("My First Streamlit Application")
+
+#Set page configuration
+st.set_page_config(layout = "wide", page_title = "Streamlit Data-centric App", page_icon = ":taxi:")
+
+
+#Provide a list of functionalities to select from
+message = """
+        __Select a functionality from the list below__
+        """
+with st.sidebar:
+    st.markdown(message)
+    page = st.selectbox('Select:',
+        ['View Data Using Dropdowns',
+        'Visualize Data on a Map',
+        '2D Charts and Histograms', 
+        '3D Charts and Histograms']) 
