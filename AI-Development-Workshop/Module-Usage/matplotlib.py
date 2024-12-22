76cb3a95cf6f2223f9fb2c9b244ecb91446beb17
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-12-22 16:07:00 +0530

    AI Dev Workshop

--- AI-Development-Workshop/Module-Usage/matplotlib.py
+++ AI-Development-Workshop/Module-Usage/matplotlib.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 days = ["Mon", "Tue", "Wed", "Thu", "Fri"]
 candies = [2, 5, 3, 6, 4]
 
-plt.plot(days, candies)
+plt.plot(days, candies) 
 plt.show()
 
 
--- AI-Development-Workshop/Module-Usage/numpy.py
+++ AI-Development-Workshop/Module-Usage/numpy.py
@@ -1,5 +1,5 @@
 # NumPy helps computers do math really fast, especially with big numbers and lists.
 import numpy as np
 
-scores = np.array([80, 90, 85, 95])
+scores = np.array([80, 90, 85, 95]) 
 print(scores + 5)  # Adds 5 to every score
